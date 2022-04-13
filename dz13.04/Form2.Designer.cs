e01c36f61a1e8f991e859b55c8b5a67f99a2911a
dasha <dasha@DASHA-PC>
2022-04-13 11:17:50 +0300

    Добавьте файлы проекта.

---
+++ dz13.04/Form2.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace dz13._04
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lastname = new System.Windows.Forms.TextBox();
+            this.firstname = new System.Windows.Forms.TextBox();
+            this.exam = new System.Windows.Forms.TextBox();
+            this.coursework = new System.Windows.Forms.TextBox();
+            this.averageball = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(434, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 199);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ФАМИЛИЯ";
+            // 
+            // lastname
+            // 
+            this.lastname.Location = new System.Drawing.Point(135, 48);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(222, 27);
+            this.lastname.TabIndex = 3;
+            // 
+            // firstname
+            // 
+            this.firstname.Location = new System.Drawing.Point(135, 110);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(199, 27);
+            this.firstname.TabIndex = 4;
+            // 
+            // exam
+            // 
+            this.exam.Location = new System.Drawing.Point(135, 242);
+            this.exam.Name = "exam";
+            this.exam.Size = new System.Drawing.Size(125, 27);
+            this.exam.TabIndex = 5;
+            // 
+            // coursework
+            // 
+            this.coursework.Location = new System.Drawing.Point(135, 295);
+            this.coursework.Name = "coursework";
+            this.coursework.Size = new System.Drawing.Size(125, 27);
+            this.coursework.TabIndex = 6;
+            // 
+            // averageball
+            // 
+            this.averageball.Location = new System.Drawing.Point(135, 373);
+            this.averageball.Name = "averageball";
+            this.averageball.Size = new System.Drawing.Size(125, 27);
+            this.averageball.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ИМЯ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Группа";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(135, 177);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Экзамен";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 272);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Курсовая работа";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(135, 343);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(206, 24);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Вычислить средний балл";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 29);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "СОХРАНИТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 448);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.averageball);
+            this.Controls.Add(this.coursework);
+            this.Controls.Add(this.exam);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label1;
+        private TextBox lastname;
+        private TextBox firstname;
+        private TextBox exam;
+        private TextBox coursework;
+        private TextBox averageball;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label4;
+        private Label label5;
+        private CheckBox checkBox1;
+        private Button button2;
+    }
+}
